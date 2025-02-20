779a4fd15c5cca8b74bdfd7bbfc38bc160eae925
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-02-20 23:01:20 +0100

    Renderer class added to manage global rendering things like RenderTargets

--- CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.cpp
+++ CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.cpp
@@ -1,308 +1,57 @@
 #include "Renderer.h"
-#include "CoffeeEngine/Renderer/Material.h"
+#include "Renderer3D.h"
 #include "CoffeeEngine/Scene/PrimitiveMesh.h"
-#include "CoffeeEngine/Renderer/DebugRenderer.h"
-#include "CoffeeEngine/Renderer/EditorCamera.h"
-#include "CoffeeEngine/Renderer/Framebuffer.h"
-#include "CoffeeEngine/Renderer/Mesh.h"
-#include "CoffeeEngine/Renderer/RendererAPI.h"
-#include "CoffeeEngine/Renderer/Shader.h"
-#include "CoffeeEngine/Renderer/Texture.h"
-#include "CoffeeEngine/Renderer/UniformBuffer.h"
 
-#include "CoffeeEngine/Embedded/ToneMappingShader.inl"
-#include "CoffeeEngine/Embedded/FinalPassShader.inl"
-#include "CoffeeEngine/Embedded/MissingShader.inl"
-
-#include <cstdint>
-#include <glm/fwd.hpp>
-#include <glm/matrix.hpp>
-#include <tracy/Tracy.hpp>
+#include <vector>
 
 namespace Coffee {
 
-    static bool s_viewportResized = false;
-    static uint32_t s_viewportWidth = 0, s_viewportHeight = 0;
+    struct CameraData
+    {
+        glm::mat4 projection = glm::mat4(1.0f);
+        glm::mat4 view = glm::mat4(1.0f);
+        glm::vec3 position = {0.0f, 0.0f, 0.0f};
+    };
 
     RendererData Renderer::s_RendererData;
-    RendererStats Renderer::s_Stats;
-    RenderSettings Renderer::s_RenderSettings;
-
-    Ref<Framebuffer> Renderer::s_MainFramebuffer;
-    Ref<Framebuffer> Renderer::s_PostProcessingFramebuffer;
-    Ref<Texture2D> Renderer::s_MainRenderTexture;
-    Ref<Texture2D> Renderer::s_EntityIDTexture;
-    Ref<Texture2D> Renderer::s_PostProcessingTexture;
-    Ref<Texture2D> Renderer::s_DepthTexture;
-
-    Ref<Mesh> Renderer::s_ScreenQuad;
-
-    Ref<Shader> Renderer::s_ToneMappingShader;
-    Ref<Shader> Renderer::s_FinalPassShader;
-
-    static Ref<Cubemap> s_EnvironmentMap;
-    static Ref<Mesh> s_SkyboxMesh;
-    static Ref<Shader> s_SkyboxShader;
 
     void Renderer::Init()
     {
-        /*std::vector<std::filesystem::path> paths = {
-            "assets/textures/skybox/right.jpg",
-            "assets/textures/skybox/left.jpg",
-            "assets/textures/skybox/top.jpg",
-            "assets/textures/skybox/bottom.jpg",
-            "assets/textures/skybox/front.jpg",
-            "assets/textures/skybox/back.jpg"
-        };
-        s_EnvironmentMap = CreateRef<Cubemap>(paths);*/
+        Renderer3D::Init();
 
-        s_EnvironmentMap = /* CreateRef<Cubemap>("assets/textures/StandardCubeMap.hdr"); */ Cubemap::Load("assets/textures/StandardCubeMap.hdr");
-
-        s_SkyboxMesh = PrimitiveMesh::CreateCube({-1.0f, -1.0f, -1.0f});
-
-        s_SkyboxShader = CreateRef<Shader>("assets/shaders/SkyboxShader.glsl");
-
-        ZoneScoped;
-
-        RendererAPI::Init();
-        DebugRenderer::Init();
-
-        s_RendererData.CameraUniformBuffer = UniformBuffer::Create(sizeof(RendererData::CameraData), 0);
-        s_RendererData.RenderDataUniformBuffer = UniformBuffer::Create(sizeof(RendererData::RenderData), 1);
-
-        Ref<Shader> missingShader = CreateRef<Shader>("MissingShader", std::string(missingShaderSource));
-        s_RendererData.DefaultMaterial = CreateRef<Material>("Missing Material", missingShader); //TODO: Port it to use the Material::Create
-
-        // TODO: This is a hack to get the missing mesh add it to the PrimitiveMesh class
-        Ref<Model> m = Model::Load("assets/models/MissingMesh.glb");
-        s_RendererData.MissingMesh = m->GetMeshes()[0];
-
-        s_MainFramebuffer = Framebuffer::Create(1280, 720, { ImageFormat::RGBA32F, ImageFormat::RGB8, ImageFormat::DEPTH24STENCIL8 });
-        s_PostProcessingFramebuffer = Framebuffer::Create(1280, 720, { ImageFormat::RGBA8 });
-
-        s_MainRenderTexture = s_MainFramebuffer->GetColorTexture(0);
-        s_EntityIDTexture = s_MainFramebuffer->GetColorTexture(1);
-        s_DepthTexture = s_MainFramebuffer->GetDepthTexture();
-
-        s_PostProcessingTexture = s_PostProcessingFramebuffer->GetColorTexture(0);
+        s_RendererData.CameraUniformBuffer = UniformBuffer::Create(sizeof(CameraData), 0);
 
         s_ScreenQuad = PrimitiveMesh::CreateQuad();
+    }
 
-        s_ToneMappingShader = CreateRef<Shader>("ToneMappingShader", std::string(toneMappingShaderSource));
-        s_FinalPassShader = CreateRef<Shader>("FinalPassShader", std::string(finalPassShaderSource));
+    void Renderer::Render()
+    {
+        for (auto& target : s_RendererData.RenderTargets)
+        {
+            
+        }
     }
 
     void Renderer::Shutdown()
     {
+        Renderer3D::Shutdown();
     }
 
-    void Renderer::BeginScene(EditorCamera& camera)
+    const RenderTarget& Renderer::GetRenderTarget(const std::string& name)
     {
-        s_Stats.DrawCalls = 0;
-        s_Stats.VertexCount = 0;
-        s_Stats.IndexCount = 0;
-
-        //I think if a render queue is implemented this is not necessary. The OnResize would work.
-        if(s_viewportResized)
+        for (auto& target : s_RendererData.RenderTargets)
         {
-            ResizeFramebuffers();
-            s_viewportResized = false;
-        }
-
-        s_RendererData.cameraData.view = camera.GetViewMatrix();
-        s_RendererData.cameraData.projection = camera.GetProjection();
-        s_RendererData.cameraData.position = camera.GetPosition();
-        s_RendererData.CameraUniformBuffer->SetData(&s_RendererData.cameraData, sizeof(RendererData::CameraData));
-
-        s_RendererData.renderData.lightCount = 0;
-    }
-
-    void Renderer::BeginScene(Camera& camera, const glm::mat4& transform)
-    {
-        s_Stats.DrawCalls = 0;
-        s_Stats.VertexCount = 0;
-        s_Stats.IndexCount = 0;
-
-        // This resize the camera to the viewport size. Think how to manage this in a better way :p
-        camera.SetViewportSize(s_viewportWidth, s_viewportHeight);
-
-        s_RendererData.cameraData.view = glm::inverse(transform);
-        s_RendererData.cameraData.projection = camera.GetProjection();
-        s_RendererData.cameraData.position = transform[3];
-        s_RendererData.CameraUniformBuffer->SetData(&s_RendererData.cameraData, sizeof(RendererData::CameraData));
-
-        s_RendererData.renderData.lightCount = 0;
-    }
-
-    void Renderer::EndScene()
-    {
-        s_MainFramebuffer->Bind();
-        s_MainFramebuffer->SetDrawBuffers({0, 1});
-
-        RendererAPI::SetClearColor({0.03f,0.03f,0.03f,1.0});
-        RendererAPI::Clear();
-
-        // Currently this is done also in the runtime, this should be done only in editor mode
-        s_EntityIDTexture->Clear({-1.0f,0.0f,0.0f,0.0f});
-
-        s_RendererData.RenderDataUniformBuffer->SetData(&s_RendererData.renderData, sizeof(RendererData::RenderData));
-
-        // Sort the render queue to minimize state changes
-
-        for(const auto& command : s_RendererData.renderQueue)
-        {
-            Material* material = command.material.get();
-
-            if(material == nullptr)
+            if (target.GetName() == name)
             {
-                material = s_RendererData.DefaultMaterial.get();
+                return target;
             }
-            
-            material->Use();
-
-            const Ref<Shader>& shader = material->GetShader();
-
-            shader->Bind();
-            shader->setMat4("model", command.transform);
-            shader->setMat3("normalMatrix", glm::transpose(glm::inverse(glm::mat3(command.transform))));
-
-            //REMOVE: This is for the first release of the engine it should be handled differently
-            shader->setBool("showNormals", s_RenderSettings.showNormals);
-
-            // Convert entityID to vec3
-            uint32_t r = (command.entityID & 0x000000FF) >> 0;
-            uint32_t g = (command.entityID & 0x0000FF00) >> 8;
-            uint32_t b = (command.entityID & 0x00FF0000) >> 16;
-            glm::vec3 entityIDVec3 = glm::vec3(r / 255.0f, g / 255.0f, b / 255.0f);
-
-            shader->setVec3("entityID", entityIDVec3);
-
-            Mesh* mesh = command.mesh.get();
-            
-            if(mesh == nullptr)
-            {
-                mesh = s_RendererData.MissingMesh.get();
-            }
-            
-            RendererAPI::DrawIndexed(mesh->GetVertexArray());
-
-            s_Stats.DrawCalls++;
-
-            s_Stats.VertexCount += mesh->GetVertices().size();
-            s_Stats.IndexCount += mesh->GetIndices().size();
         }
 
-        // Test drawing the skybox
-        RendererAPI::SetDepthMask(false);
-        s_SkyboxShader->Bind();
-        RendererAPI::DrawIndexed(s_SkyboxMesh->GetVertexArray());
-        RendererAPI::SetDepthMask(true);
-
-        if(s_RenderSettings.PostProcessing)
-        {
-            //Render All the fancy effects :D
-
-            //ToneMapping
-            s_PostProcessingFramebuffer->Bind();
-
-            s_ToneMappingShader->Bind();
-            s_ToneMappingShader->setInt("screenTexture", 0);
-            s_ToneMappingShader->setFloat("exposure", s_RenderSettings.Exposure);
-            s_MainRenderTexture->Bind(0);
-
-            RendererAPI::DrawIndexed(s_ScreenQuad->GetVertexArray());
-
-            s_ToneMappingShader->Unbind();
-
-            //This has to be set because the s_ScreenQuad overwrites the depth buffer
-            RendererAPI::SetDepthMask(false);
-
-            //Final Pass
-            s_MainFramebuffer->Bind();
-            s_MainFramebuffer->SetDrawBuffers({0});
-            
-            s_FinalPassShader->Bind();
-            s_FinalPassShader->setInt("screenTexture", 0);
-            s_PostProcessingTexture->Bind(0);
-
-            RendererAPI::DrawIndexed(s_ScreenQuad->GetVertexArray());
-
-            s_FinalPassShader->Unbind();
-
-            RendererAPI::SetDepthMask(true);
-        }
-
-        DebugRenderer::Flush();
-
-        //Final Pass
-        s_RendererData.RenderTexture = s_MainRenderTexture;
-
-        s_MainFramebuffer->UnBind();
-
-        s_RendererData.renderQueue.clear();
+        return s_RendererData.RenderTargets[0];
     }
 
-    //TEMPORAL
-    void Renderer::BeginOverlay(EditorCamera& camera)
+    void Renderer::AddRenderTarget(const RenderTarget& renderTarget)
     {
-        s_RendererData.cameraData.view = camera.GetViewMatrix();
-        s_RendererData.cameraData.projection = camera.GetProjection();
-        s_RendererData.cameraData.position = camera.GetPosition();
-        s_RendererData.CameraUniformBuffer->SetData(&s_RendererData.cameraData, sizeof(RendererData::CameraData));
-
-        s_MainFramebuffer->Bind();
-    }
-
-    void Renderer::EndOverlay()
-    {
-        s_MainFramebuffer->UnBind();
-    }
-
-    void Renderer::Submit(const LightComponent& light)
-    {
-        s_RendererData.renderData.lights[s_RendererData.renderData.lightCount] = light;
-        s_RendererData.renderData.lightCount++;
-    }
-
-    void Renderer::Submit(const RenderCommand& command)
-    {
-        s_RendererData.renderQueue.push_back(command);
-    }
-
-    // Temporal, this should be removed because this is rendering immediately.
-    void Renderer::Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, const glm::mat4& transform, uint32_t entityID)
-    {
-        shader->Bind();
-        shader->setMat4("model", transform);
-        shader->setMat3("normalMatrix", glm::transpose(glm::inverse(glm::mat3(transform))));
-
-        //REMOVE: This is for the first release of the engine it should be handled differently
-        shader->setBool("showNormals", s_RenderSettings.showNormals);
-
-        // Convert entityID to vec3
-        uint32_t r = (entityID & 0x000000FF) >> 0;
-        uint32_t g = (entityID & 0x0000FF00) >> 8;
-        uint32_t b = (entityID & 0x00FF0000) >> 16;
-        glm::vec3 entityIDVec3 = glm::vec3(r / 255.0f, g / 255.0f, b / 255.0f);
-
-        shader->setVec3("entityID", entityIDVec3);
-
-        RendererAPI::DrawIndexed(vertexArray);
-
-        s_Stats.DrawCalls++;
-    }
-
-    void Renderer::OnResize(uint32_t width, uint32_t height)
-    {
-        s_viewportWidth = width;
-        s_viewportHeight = height;
-
-        s_viewportResized = true;
-    }
-
-    void Renderer::ResizeFramebuffers()
-    {
-        s_MainFramebuffer->Resize(s_viewportWidth, s_viewportHeight);
-        s_PostProcessingFramebuffer->Resize(s_viewportWidth, s_viewportHeight);
+        s_RendererData.RenderTargets.push_back(renderTarget);
     }
 }
--- CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.h
+++ CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.h
@@ -1,219 +1,34 @@
 #pragma once
 
-#include "CoffeeEngine/Core/Base.h"
-#include "CoffeeEngine/Renderer/EditorCamera.h"
-#include "CoffeeEngine/Renderer/Framebuffer.h"
-#include "CoffeeEngine/Renderer/Material.h"
 #include "CoffeeEngine/Renderer/Mesh.h"
-#include "CoffeeEngine/Renderer/Shader.h"
-#include "CoffeeEngine/Renderer/Texture.h"
+#include "CoffeeEngine/Renderer/RenderTarget.h"
 #include "CoffeeEngine/Renderer/UniformBuffer.h"
-#include "CoffeeEngine/Renderer/VertexArray.h"
-#include "CoffeeEngine/Scene/Components.h"
-#include <glm/fwd.hpp>
-
+#include <vector>
 namespace Coffee {
 
-    /**
-     * @defgroup renderer Renderer
-     * @brief Renderer components of the CoffeeEngine.
-     * @{
-     */
-
-    struct RenderCommand
-    {
-        glm::mat4 transform;
-        Ref<Mesh> mesh;
-        Ref<Material> material;
-        uint32_t entityID;
-    };
-
-    /**
-     * @brief Structure containing renderer data.
-     */
     struct RendererData
     {
-        /**
-         * @brief Structure containing camera data.
-         */
-        struct CameraData
-        {
-            glm::mat4 projection; ///< The projection matrix.
-            glm::mat4 view; ///< The view matrix.
-            glm::vec3 position; ///< The position of the camera.
-        };
-
-        /**
-         * @brief Structure containing render data.
-         */
-        struct RenderData
-        {
-            LightComponent lights[32]; ///< Array of light components.
-            int lightCount = 0; ///< Number of lights.
-        };
-
-        CameraData cameraData; ///< Camera data.
-        RenderData renderData; ///< Render data.
-
+        std::vector<RenderTarget> RenderTargets;
+        
         Ref<UniformBuffer> CameraUniformBuffer; ///< Uniform buffer for camera data.
-        Ref<UniformBuffer> RenderDataUniformBuffer; ///< Uniform buffer for render data.
-
-        Ref<Material> DefaultMaterial; ///< Default material.
-        Ref<Mesh> MissingMesh; ///< Missing mesh.
-
-        Ref<Texture2D> RenderTexture; ///< Render texture.
-
-        std::vector<RenderCommand> renderQueue; ///< Render queue.
     };
 
-    /**
-     * @brief Structure containing renderer statistics.
-     */
-    struct RendererStats
-    {
-        uint32_t DrawCalls = 0; ///< Number of draw calls.
-        uint32_t VertexCount = 0; ///< Number of vertices.
-        uint32_t IndexCount = 0; ///< Number of indices.
-    };
-
-    /**
-     * @brief Structure containing render settings.
-     */
-    struct RenderSettings
-    {
-        bool PostProcessing = true; ///< Enable or disable post-processing.
-        bool SSAO = false; ///< Enable or disable SSAO.
-        bool Bloom = false; ///< Enable or disable bloom.
-        bool FXAA = false; ///< Enable or disable FXAA.
-        float Exposure = 1.0f; ///< Exposure value.
-
-        // REMOVE: This is for the first release of the engine it should be handled differently
-        bool showNormals = false;
-    };
-
-    /**
-     * @brief Class representing the 3D renderer.
-     */
     class Renderer
     {
     public:
-        /**
-         * @brief Initializes the renderer.
-         */
+        
         static void Init();
-
-        /**
-         * @brief Shuts down the renderer.
-         */
+        static void Render();
         static void Shutdown();
 
-        /**
-         * @brief Begins a new scene with the specified editor camera.
-         * @param camera The editor camera.
-         */
-        static void BeginScene(EditorCamera& camera);
+        static const RenderTarget& GetRenderTarget(const std::string& name);
+        static void AddRenderTarget(const RenderTarget& renderTarget);
 
-        /**
-         * @brief Begins a new scene with the specified camera and transform.
-         * @param camera The camera.
-         * @param transform The transform matrix.
-         */
-        static void BeginScene(Camera& camera, const glm::mat4& transform);
-
-        /**
-         * @brief Ends the current scene.
-         */
-        static void EndScene();
-
-        /**
-         * @brief Begins an overlay with the specified editor camera.
-         * @param camera The editor camera.
-         */
-        static void BeginOverlay(EditorCamera& camera);
-
-        /**
-         * @brief Ends the current overlay.
-         */
-        static void EndOverlay();
-
-        static void Submit(const RenderCommand& command);
-
-        static void Submit(const Ref<Shader>& shader, const Ref<VertexArray>& vertexArray, const glm::mat4& transform = glm::mat4(1.0f), uint32_t entityID = 4294967295);
-
-        /**
-         * @brief Submits a light component.
-         * @param light The light component.
-         */
-
-         //Todo change this to a light class and not a component
-        static void Submit(const LightComponent& light);
-
-        /**
-         * @brief Resizes the renderer to the specified width and height.
-         * @param width The new width.
-         * @param height The new height.
-         */
-        static void OnResize(uint32_t width, uint32_t height);
-
-        /**
-         * @brief Gets the render texture.
-         * @return A reference to the render texture.
-         */
-        static const Ref<Texture2D>& GetRenderTexture() { return s_RendererData.RenderTexture; }
-
-        /**
-         * @brief Retrieves the texture associated with the entity ID.
-         * 
-         * This static method returns a reference to the texture that is used to 
-         * identify entities within the renderer. The texture is stored as a 
-         * static member of the class.
-         * 
-         * @return A constant reference to the entity ID texture.
-         */
-        static const Ref<Texture2D>& GetEntityIDTexture() { return s_EntityIDTexture; }
-
-        static glm::vec4 GetEntityIDAtPixel(int x, int y) { return s_MainFramebuffer->GetPixelColor(x, y, 1); }
-
-        /**
-         * @brief Gets the renderer data.
-         * @return A reference to the renderer data.
-         */
-        static const RendererData& GetData() { return s_RendererData; }
-
-        /**
-         * @brief Gets the renderer statistics.
-         * @return A reference to the renderer statistics.
-         */
-        static const RendererStats& GetStats() { return s_Stats; }
-
-        /**
-         * @brief Gets the render settings.
-         * @return A reference to the render settings.
-         */
-        static RenderSettings& GetRenderSettings() { return s_RenderSettings; }
-
+        static std::vector<RenderTarget>& GetRenderTargets() { return s_RendererData.RenderTargets; }
     private:
-
-        static void ResizeFramebuffers();
-
-    private:
-        static RendererData s_RendererData; ///< Renderer data.
-        static RendererStats s_Stats; ///< Renderer statistics.
-        static RenderSettings s_RenderSettings; ///< Render settings.
-
-        static Ref<Texture2D> s_MainRenderTexture; ///< Main render texture.
-        static Ref<Texture2D> s_EntityIDTexture; ///< Entity ID texture.
-        static Ref<Texture2D> s_PostProcessingTexture; ///< Post-processing texture.
-        static Ref<Texture2D> s_DepthTexture; ///< Depth texture.
-
-        static Ref<Framebuffer> s_MainFramebuffer; ///< Main framebuffer.
-        static Ref<Framebuffer> s_PostProcessingFramebuffer; ///< Post-processing framebuffer.
+        static RendererData s_RendererData;
 
         static Ref<Mesh> s_ScreenQuad; ///< Screen quad mesh.
-
-        static Ref<Shader> s_ToneMappingShader; ///< Tone mapping shader.
-        static Ref<Shader> s_FinalPassShader; ///< Final pass shader.
     };
 
-    /** @} */
 }
