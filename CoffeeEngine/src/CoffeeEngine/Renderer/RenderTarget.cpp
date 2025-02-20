a9c051d3a0d77c3bb89d8912f621784df380699f
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-02-20 23:00:12 +0100

    RenderTarget Constructor parameter modifications + CameraData Added

--- CoffeeEngine/src/CoffeeEngine/Renderer/RenderTarget.cpp
+++ CoffeeEngine/src/CoffeeEngine/Renderer/RenderTarget.cpp
@@ -2,9 +2,13 @@
 
 namespace Coffee {
 
-    RenderTarget::RenderTarget(const std::string& name, const glm::vec2& size)
+    RenderTarget::RenderTarget(const std::string& name, const glm::vec2& size, const std::vector<std::initializer_list<Attachment>>& framebufferAttachments)
         : m_Name(name), m_Size(size)
     {
+        for (const auto& attachments : framebufferAttachments)
+        {
+            m_Framebuffers.push_back(Framebuffer::Create(size.x, size.y, attachments));
+        }
     }
 
     const Ref<Framebuffer>& RenderTarget::GetFramebuffer(const std::string& name) const
--- CoffeeEngine/src/CoffeeEngine/Renderer/RenderTarget.h
+++ CoffeeEngine/src/CoffeeEngine/Renderer/RenderTarget.h
@@ -8,18 +8,18 @@
 
 namespace Coffee {
     
+    struct CameraData
+    {
+        glm::mat4 projection; ///< The projection matrix.
+        glm::mat4 view; ///< The view matrix.
+        glm::vec3 position; ///< The position of the camera.
+    };
+
     class RenderTarget
     {
     public:
         RenderTarget() = default;
-        RenderTarget(const std::string& name, const glm::vec2& size, const std::vector<std::initializer_list<Attachment>>& framebufferAttachments)
-            : m_Name(name), m_Size(size)
-        {
-            for (const auto& attachments : framebufferAttachments)
-            {
-                m_Framebuffers.push_back(Framebuffer::Create(size.x, size.y, attachments));
-            }
-        }
+        RenderTarget(const std::string& name, const glm::vec2& size, const std::vector<std::initializer_list<Attachment>>& framebufferAttachments);
         ~RenderTarget() = default;
 
         const std::vector<Ref<Framebuffer>>& GetFramebuffers() const { return m_Framebuffers; }
@@ -27,11 +27,14 @@ namespace Coffee {
 
         const std::string& GetName() const { return m_Name; }
         void SetName(const std::string& name) { m_Name = name; }
+
+        void SetCameraData(const CameraData& cameraData) { m_CameraData = cameraData; }
+        const CameraData& GetCameraData() const { return m_CameraData; }
     
     private:
         std::string m_Name;
 
-        // Camera?
+        CameraData m_CameraData;
 
         glm::vec2 m_Size;
         std::vector<Ref<Framebuffer>> m_Framebuffers;
