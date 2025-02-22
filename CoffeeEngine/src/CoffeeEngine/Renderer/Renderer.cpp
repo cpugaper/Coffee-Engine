9534f59344cf9547bfdf6e3785c8b17563517965
Hugo Planell <hugoplanellmoreno@gmail.com>
2025-02-22 11:36:47 +0100

    Renderer RenderTarget new functionality working

--- CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.cpp
+++ CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.cpp
@@ -1,22 +1,22 @@
 #include "Renderer.h"
 #include "Renderer3D.h"
+#include "CoffeeEngine/Renderer/RendererAPI.h"
 #include "CoffeeEngine/Scene/PrimitiveMesh.h"
 
+#include <glm/matrix.hpp>
 #include <vector>
 
 namespace Coffee {
-
-    struct CameraData
-    {
-        glm::mat4 projection = glm::mat4(1.0f);
-        glm::mat4 view = glm::mat4(1.0f);
-        glm::vec3 position = {0.0f, 0.0f, 0.0f};
-    };
-
+    
     RendererData Renderer::s_RendererData;
+    RendererSettings Renderer::s_RenderSettings;
+
+    Ref<Mesh> Renderer::s_ScreenQuad;
 
     void Renderer::Init()
     {
+        RendererAPI::Init();
+
         Renderer3D::Init();
 
         s_RendererData.CameraUniformBuffer = UniformBuffer::Create(sizeof(CameraData), 0);
@@ -28,8 +28,24 @@ namespace Coffee {
     {
         for (auto& target : s_RendererData.RenderTargets)
         {
-            
+            CameraData cameraData;
+            cameraData.view = glm::inverse(target.GetCameraTransform());
+            cameraData.projection = target.GetCamera().GetProjection();
+            cameraData.position = target.GetCameraTransform()[3];
+
+            s_RendererData.CameraUniformBuffer->SetData(&cameraData, sizeof(CameraData));
+
+            Renderer3D::ForwardPass(target);
+            Renderer3D::SkyboxPass(target);
+
+            if(s_RenderSettings.PostProcessing)
+            {
+                Renderer3D::PostProcessingPass(target);
+            }
         }
+
+        // TODO: Think if this should be done here or inside each target?
+        Renderer3D::ResetCalls();
     }
 
     void Renderer::Shutdown()
@@ -50,8 +66,9 @@ namespace Coffee {
         return s_RendererData.RenderTargets[0];
     }
 
-    void Renderer::AddRenderTarget(const RenderTarget& renderTarget)
+    RenderTarget& Renderer::AddRenderTarget(const std::string& name, const glm::vec2& size, const std::vector<std::pair<std::string, std::initializer_list<Attachment>>>& framebufferAttachments)
     {
-        s_RendererData.RenderTargets.push_back(renderTarget);
+        s_RendererData.RenderTargets.push_back(RenderTarget(name, size, framebufferAttachments));
+        return s_RendererData.RenderTargets.back();
     }
 }
--- CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.h
+++ CoffeeEngine/src/CoffeeEngine/Renderer/Renderer.h
@@ -3,14 +3,33 @@
 #include "CoffeeEngine/Renderer/Mesh.h"
 #include "CoffeeEngine/Renderer/RenderTarget.h"
 #include "CoffeeEngine/Renderer/UniformBuffer.h"
+#include <glm/fwd.hpp>
 #include <vector>
 namespace Coffee {
+
+    struct CameraData
+    {
+        glm::mat4 projection = glm::mat4(1.0f); ///< The projection matrix.
+        glm::mat4 view = glm::mat4(1.0f); ///< The view matrix.
+        glm::vec3 position = {0.0, 0.0, 0.0}; ///< The position of the camera.
+    };
 
     struct RendererData
     {
         std::vector<RenderTarget> RenderTargets;
-        
+        RenderTarget* CurrentRenderTarget = nullptr;
+
         Ref<UniformBuffer> CameraUniformBuffer; ///< Uniform buffer for camera data.
+        CameraData cameraData; ///< Camera data.
+    };
+
+    struct RendererStats
+    {
+    };
+
+    struct RendererSettings
+    {
+        bool PostProcessing = true; ///< Enable or disable post-processing.
     };
 
     class Renderer
@@ -22,12 +41,18 @@ namespace Coffee {
         static void Shutdown();
 
         static const RenderTarget& GetRenderTarget(const std::string& name);
-        static void AddRenderTarget(const RenderTarget& renderTarget);
+        static RenderTarget& AddRenderTarget(const std::string& name, const glm::vec2& size, const std::vector<std::pair<std::string, std::initializer_list<Attachment>>>& framebufferAttachments);
+
+        static RenderTarget* GetCurrentRenderTarget() { return s_RendererData.CurrentRenderTarget; }
+        static void SetCurrentRenderTarget(RenderTarget* renderTarget) { s_RendererData.CurrentRenderTarget = renderTarget; }
 
         static std::vector<RenderTarget>& GetRenderTargets() { return s_RendererData.RenderTargets; }
-    private:
-        static RendererData s_RendererData;
 
+        static RendererSettings& GetRenderSettings() { return s_RenderSettings; }
+
+    private:
+        static RendererData s_RendererData; ///< Renderer data.
+        static RendererSettings s_RenderSettings; ///< Render settings.
         static Ref<Mesh> s_ScreenQuad; ///< Screen quad mesh.
     };
 
